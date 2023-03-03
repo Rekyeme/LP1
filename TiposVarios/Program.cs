bf7980104f20fc9fc21d68e7f4c63c259405aac9
Rekyeme <samuelcarvalho.9966@gmail.com>
2023-03-03 11:08:18 +0000

    Variaveis Inteiras

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Variaveis Inteiras");
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
