c4f82bc95f6a950c1b2433850439fa2c99f737cb
Rekyeme <samuelcarvalho.9966@gmail.com>
2023-03-03 11:46:40 +0000

    Reais

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -10,15 +10,22 @@ namespace TiposVarios
             string cs = "\u00A9";
             string cd = "\u00A7";
             string cf = "\u00A6";
-            Console.WriteLine("Variaveis Inteiras");
+            float A = 19.11f;
+            double AA = 87.45e-79;
             for (int i = 0; i < 3; i++)
             {
                 Console.WriteLine(i);
             }
+             Console.WriteLine("Variaveis Inteiras");
             Console.WriteLine(ca);
             Console.WriteLine(cs);
             Console.WriteLine(cd);
             Console.WriteLine(cf);
+            Console.WriteLine("Reais");
+            Console.WriteLine(A);
+            Console.WriteLine(AA);
+            
+
         }
     }
 }
