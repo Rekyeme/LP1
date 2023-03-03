b76a65246ad783832804962c9b5a3a324b62cd25
Rekyeme <samuelcarvalho.9966@gmail.com>
2023-03-03 11:38:15 +0000

    Unicode

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -6,11 +6,19 @@ namespace TiposVarios
     {
         static void Main(string[] args)
         {
+            string ca = "\u00A9’";
+            string cs = "\u00A9";
+            string cd = "\u00A7";
+            string cf = "\u00A6";
             Console.WriteLine("Variaveis Inteiras");
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < 3; i++)
             {
                 Console.WriteLine(i);
             }
+            Console.WriteLine(ca);
+            Console.WriteLine(cs);
+            Console.WriteLine(cd);
+            Console.WriteLine(cf);
         }
     }
 }
