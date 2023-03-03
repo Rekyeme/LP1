19d37d7e5215fe46c9427e3a02ce3c7168c5009b
Rekyeme <samuelcarvalho.9966@gmail.com>
2023-03-03 10:56:00 +0000

    XML Documentation

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -2,8 +2,15 @@
 
 namespace OlaMundo
 {
+    /// <summary>
+    /// Class for program initialization
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Main function to write the commands
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
