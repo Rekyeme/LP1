bae728598ac1f44570b8cfd5f2ec5c3c5a295027
Rekyeme <samuelcarvalho.9966@gmail.com>
2023-03-03 12:04:35 +0000

    Varias Strings

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VariasStrigs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tt = "Yupi";
+            string oo = "Yahahahahah";
+            string pp = @"Bom dia";
+            string gg = @"Boa noite";
+
+            Console.WriteLine(tt);
+            Console.WriteLine(oo);
+            Console.WriteLine(pp);
+            Console.WriteLine(gg);
+        }
+    }
+}
